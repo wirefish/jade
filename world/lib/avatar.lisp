78e855adee4a9bf5194412b27c70a5547bbbf696
Craig Becker <craig@wirefish.com>
2023-08-02 07:44:36 -0700

    add race, level, xp to new avatar

--- world/lib/avatar.lisp
+++ world/lib/avatar.lisp
@@ -1,4 +1,10 @@
 (in-package :jade.lib)
 
+(defentity reborn-spirit ()
+  (:brief "a reborn spirit"
+   :icon 'ghost))
+
 (defentity new-avatar (avatar)
-  ())
+  (:race reborn-spirit
+   :level 0
+   :xp 0))
