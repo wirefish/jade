4aeab399730280ceda8bfc17af5777c5cea2e71d
Craig Becker <craig@wirefish.com>
2023-08-08 08:55:43 -0700

    add log-run-time macro

--- src/logging.lisp
+++ src/logging.lisp
@@ -11,3 +11,12 @@
     (multiple-value-bind (second minute hour date month) (get-decoded-time)
       (format *log-stream* "~c ~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d ~?~%"
               (char (string level) 0) month date hour minute second control-string args))))
+
+(defmacro log-run-time (level label &body body)
+  (with-gensyms (timer)
+    `(let ((,timer (get-internal-run-time)))
+       (prog1
+           (progn ,@body)
+         (format-log ,level "~a took ~,6f sec" ,label
+                     (/ (- (get-internal-run-time) ,timer)
+                        internal-time-units-per-second))))))
