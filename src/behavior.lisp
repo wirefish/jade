3e7c0f5fa4cb09cedfd025c13081df596c66f803
Craig Becker <craig@wirefish.com>
2023-08-12 15:23:00 -0700

    simplify event handling

--- src/behavior.lisp
+++ src/behavior.lisp
@@ -14,15 +14,17 @@
 ;;; witness and react to the action.
 ;;;
 ;;; Every action is broken down into four `phases'. Each phase triggers a
-;;; corresponding `event'. The phases are `allow', `before', `do', and `after'.
+;;; corresponding `event'. The phases are `allow', `before', `when', and `after'.
 ;;; Events are named by keywords that combine the names of the phase and action,
 ;;; e.g. :before-enter-location.
 ;;;
-;;; For each phase, all observers (or participants in the case of the do phase)
-;;; have the opportunity to react to the event. This is done by defining an
-;;; appropriate `event-handler' within `defentity' or `defbehavior'.
+;;; For each phase, all observers (or participants in the case of the when
+;;; phase) have the opportunity to react to the event. This is done by defining
+;;; an appropriate event handler within `defentity' or `defbehavior'.
 ;;;
 ;;; An entity's `behavior' is simply an ordered collection of event handlers.
+;;; The first handler that matches the event name and arguments is called to
+;;; handle the event.
 ;;;
 ;;; The event phases work as follows:
 ;;;
@@ -32,42 +34,25 @@
 ;;; * before: Handlers in this phase are called before the action actually
 ;;;   happens, but after it is known that it will happen.
 ;;;
-;;; * do: Handlers in this phase are called only on the particpants, and should
-;;;   make any state changes necessary to implement the action.
+;;; * when: Handlers in this phase are called only on the particpants, and
+;;;   should make any state changes necessary to implement the action.
 ;;;
 ;;; * after: Handlers in this phase are called after the action has completed.
 ;;;
 ;;; In all phases, a handler can call `call-next-handler' to pass control to the
-;;; next matching handler.
+;;; next matching handler, if any.
 
-(defstruct event-handler
-  "Combines a `test' function that checks if this handler applies to specific
-event arguments, and a `fn' function that implements the response to the event."
-  test fn)
-
-(defun push-event-handler (entity event test fn)
+(defun push-event-handler (entity event fn)
   "Adds an event handler to the front of the list of an entity's handlers for a
 specific event."
   (with-slots (behavior) entity
     (when (null behavior)
       (setf behavior (make-hash-table)))
-    (push (make-event-handler :test test :fn fn) (gethash event behavior))))
+    (push fn (gethash event behavior))))
 
 (defun allow-phase-p (event)
   "Returns t if `event' names an event in the allow phase."
   (string-starts-with (symbol-name event) "ALLOW-"))
-
-(defun make-event-handler-fn (event params body)
-  "Returns a lambda expression that implements the body of an event handler."
-  `(lambda (self ,@params)
-     (declare (ignorable self ,@params))
-     (block handler-body
-       (labels (,@(when (tree-contains 'call-next-handler body)
-                    '((call-next-handler () (return-from handler-body :call-next-handler))))
-                ,@(when (and (allow-phase-p event)
-                             (tree-contains 'disallow-action body))
-                    '((disallow-action () (return-from handler-body :disallow-action)))))
-         ,@body))))
 
 (defun make-constraint-test (observer param constraint)
   "Returns an expression that evaluates to true if the value of `param'
@@ -89,35 +74,40 @@ satisfies `constraint'."
     (t
      (error "invalid constraint ~a" constraint))))
 
-(defun make-event-handler-test (params constraints)
-  "Returns a lambda expression that implements the test function for an event
-handler."
-  (let* ((observer (gensym))
-         (tests (loop for param in params for constraint in constraints when constraint
-                     collect (make-constraint-test observer param constraint))))
-    `(lambda (,observer ,@params)
-       (declare (ignorable ,observer ,@params))
-       ,(if tests `(and ,@tests) t))))
+(defun make-event-handler (event params body)
+  (let ((tests (loop for (name . constraint) in params
+                     when constraint
+                       collect (make-constraint-test 'self name constraint)))
+        (param-names (mapcar #'car params))
+        (handler-body (gensym)))
+    `(lambda (self ,@param-names)
+       (declare (ignorable self ,@param-names))
+       (block ,handler-body
+         (labels (,@(when (or tests (tree-contains 'call-next-handler body))
+                      `((call-next-handler () (return-from ,handler-body :call-next-handler))))
+                  ,@(when (and (allow-phase-p event)
+                               (tree-contains 'disallow-action body))
+                      `((disallow-action () (return-from ,handler-body :disallow-action)))))
+           ,@(when tests
+               `((unless (and ,@tests)
+                   (call-next-handler))))
+           ,@body)))))
 
-(defun anonymize-parameters (param-specs)
-  (loop for spec in param-specs
-        collect (if (eq spec 'self)
-                    (list (gensym) 'self)
-                    spec)))
+(defun normalize-parameters (params)
+  (loop for param in params
+        collect (cond
+                  ((eq param 'self) (list (gensym) 'self))
+                  ((symbolp param) (list param))
+                  (t param))))
 
 (defmacro defbehavior (entity &body clauses)
-  (labels ((rest-or-nil (x) (when (listp x) (rest x)))
-           (first-or-self (x) (if (listp x) (first x) x)))
-    `(progn
-       ,@(loop for (event spec . body) in (reverse clauses)
-               collect (let* ((spec (anonymize-parameters spec))
-                              (constraints (mapcar #'rest-or-nil spec))
-                              (params (mapcar #'first-or-self spec)))
-                         `(push-event-handler
-                           ,entity ',event
-                           ,(make-event-handler-test params constraints)
-                           ,(make-event-handler-fn event params body))))
-       ,entity)))
+  `(progn
+     ,@(loop for (event params . body) in (reverse clauses)
+             collect
+             `(push-event-handler
+               ,entity ',event
+               ,(make-event-handler event (normalize-parameters params) body)))
+     ,entity))
 
 (defgeneric observe-event (observer event &rest args))
 
@@ -125,12 +115,17 @@ handler."
   (declare (ignore args)))
 
 (defmethod observe-event ((observer entity) event &rest args)
-  (let ((handlers (? observer 'behavior event)))
-    (loop for handler in handlers do
-      (when (apply (event-handler-test handler) observer args)
-        (let ((x (apply (event-handler-fn handler) observer args)))
-          (unless (eq x :call-next-handler)
-            (return-from observe-event x)))))))
+  (let ((fns (? observer 'behavior event)))
+    (loop for fn in fns do
+      (let ((x (apply fn observer args)))
+        (unless (eq x :call-next-handler)
+          (return-from observe-event x))))))
+
+(defun observers-allow-p (observers event &rest args)
+  (dolist (observer observers)
+    (when (eq (apply #'observe-event observer event args) :disallow-action)
+      (return-from observers-allow-p nil)))
+  t)
 
 (defun notify-observers (observers event &rest args)
   (dolist (observer observers)
@@ -152,12 +147,6 @@ appropriate describes an action taken by `actor'."
     (show observer (if (functionp message) (funcall message observer) message))
     (when event-args
       (apply #'observe-event observer event-args))))
-
-(defun observers-allow-p (observers event &rest args)
-  (dolist (observer observers)
-    (when (eq (apply #'observe-event observer event args) :disallow-action)
-      (return-from observers-allow-p nil)))
-  t)
 
 (defun reacts-to-event-p (observer event)
   (not (null (? observer 'behavior event))))
