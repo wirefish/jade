f1cf8b37e02c942d133411b8b7def212eebd114b
Craig Becker <craig@wirefish.com>
2023-08-20 08:44:02 -0700

    add random delays and intervals

--- src/random.lisp
+++ src/random.lisp
@@ -1,5 +1,7 @@
 (in-package :jade)
 
+;;; Basic random number generation.
+
 (declaim (ftype (function (float) integer) round-random))
 (defun round-random (n)
   "Returns a random integer that is either floor(n) or ceil(n), with probability
@@ -14,3 +16,26 @@
 (declaim (ftype (function (integer integer) integer) random-int))
 (defun random-integer (min max)
   (+ min (random (1+ (- max min)))))
+
+;;; Wrappers around cl-async delays and intervals to support random timing.
+
+(defmacro with-random-delay ((min max) &body body)
+  `(as:with-delay ((random-integer ,min ,max))
+     ,@body))
+
+(defun random-interval (callback &key min max event-cb)
+  (let (event)
+    (labels ((main ()
+               (funcall callback)
+               (when event
+                 (setf event (as:delay #'main :time (random-integer min max)
+                                              :event-cb event-cb)))))
+      (setf event (as:delay #'main :time (random-integer min max)
+                                   :event-cb event-cb))
+      (lambda ()
+        (when event
+          (as:remove-event event)
+          (setf event nil))))))
+
+(defmacro with-random-interval ((min max) &body body)
+  `(random-interval (lambda () ,@body) :min ,min :max ,max))
