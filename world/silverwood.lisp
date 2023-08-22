f88ca17d769da2628015ad17d70f26fd8d6c8a0c
Craig Becker <craig@wirefish.com>
2023-08-21 18:07:15 -0700

    add link to copper mine

--- world/silverwood.lisp
+++ world/silverwood.lisp
@@ -173,7 +173,8 @@
   (:exits ((forest-portal :south forest-E01 :east forest-F00))))
 
 (deflocation forest-F00 (forest)
-  (:exits ((forest-portal :west forest-E00 :south forest-F01 :east forest-G00))))
+  (:exits ((forest-portal :west forest-E00 :south forest-F01 :east forest-G00)
+           (narrow-track :north jade.copper-mine::entrance))))
 
 (deflocation forest-G00 (forest)
   (:exits ((forest-portal :west forest-F00 :south forest-G01 :east forest-H00))))
